87052a1a5865f26bb892248b3d06729226af5ea7
Donny-Hikari <donny.hikari@gmail.com>
2018-03-30 00:41:57 +0900

    Initial commit.
    Implement AdaBoostClassifier and DecisionStumpClassifier.

---
+++ adaboost/__init__.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+
+from .adaboost import AdaBoostClassifier
+from .decisionstump import DecisionStumpClassifier
+
+from . import adaboost
+
+__all__ = [
+    "AdaBoostClassifier",
+    "DecisionStumpClassifier"
+]
